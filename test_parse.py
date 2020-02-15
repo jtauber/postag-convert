3bbace71bde2a13e9b56ec4c93ca6383058498c7
James Tauber <jtauber@jtauber.com>
2020-02-15 02:44:39 -0500

    show test status

--- test_parse.py
+++ test_parse.py
@@ -8,19 +8,24 @@ from patterns.morphgnt import REGEXES as MORPHGNT_REGEXES
 from patterns.morphgnt2 import REGEXES as MORPHGNT2_REGEXES
 
 
-def test_parse(regexes, test_filename):
+def test_parse(label, regexes, test_filename):
+    print(label, end="...", flush=True)
+    count = 0
     with open(test_filename) as f:
         for line in f:
+            count += 1
             postag = line.strip()
 
             for regex in regexes:
                 if m := re.match(regex + "$", postag):
                     break
             else:
+                print(ERROR)
                 print(postag)
                 quit()
+    print(f"{count} PASSED.")
 
 
-test_parse(ROBINSON_REGEXES, "examples/robinson.txt")
-test_parse(MORPHGNT_REGEXES, "examples/morphgnt.txt")
-test_parse(MORPHGNT2_REGEXES, "examples/morphgnt2.txt")
+test_parse("robinson", ROBINSON_REGEXES, "examples/robinson.txt")
+test_parse("morphgnt", MORPHGNT_REGEXES, "examples/morphgnt.txt")
+test_parse("morphgnt2", MORPHGNT2_REGEXES, "examples/morphgnt2.txt")
