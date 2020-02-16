d03fa67c63e34f46f6b1f4a941a85cf4497f5ee7
James Tauber <jtauber@jtauber.com>
2020-02-16 03:47:44 -0500

    finish initial mapping of morphgnt2

--- patterns/morphgnt2.py
+++ patterns/morphgnt2.py
@@ -1,11 +1,13 @@
-CASE = "[NAVGDC]"
-GENDER = "[FMNXYZ]"
-PERSON = "(?P<person>[123])"
-NUMBER = "(?P<number>[SP])"
-VOICE = "[AMP]"
-TENSE = "[PIFAXY]"
-TENSE_1 = "[PFAX]"
-MOOD_F = "[DISO]"
+CASE = r"(?P<case>[NAVGDC])"
+GENDER = r"(?P<gender>[FMNXYZ])"
+PERSON = r"(?P<person>[123])"
+NUMBER = r"(?P<number>[SP])"
+VOICE = r"(?P<voice>[AMP])"
+TENSE = r"(?P<tense>[PIFAXY])"
+TENSE_1 = r"(?P<tense>[PFAX])"
+MOOD_F = r"(?P<mood>[DISO])"
+MOOD_N = r"(?P<mood>N)"
+MOOD_P = r"(?P<mood>P)"
 
 CN_ = f"{CASE}{NUMBER}-"
 CNG = f"{CASE}{NUMBER}{GENDER}"
@@ -15,6 +17,6 @@ REGEXES = [
     f"----{CN_}",
     f"----{CNG}",
     f"{TENSE}{VOICE}{MOOD_F}{PERSON}-{NUMBER}-",
-    f"{TENSE_1}{VOICE}N----",
-    f"{TENSE_1}{VOICE}P-{CNG}",
+    f"{TENSE_1}{VOICE}{MOOD_N}----",
+    f"{TENSE_1}{VOICE}{MOOD_P}-{CNG}",
 ]
--- prototype_mapping.py
+++ prototype_mapping.py
@@ -7,23 +7,118 @@ from patterns.morphgnt2 import REGEXES as MORPHGNT2_REGEXES
 
 
 MAPPINGS = {
+    "case": {
+        1: {  # nominative
+            "morphgnt2": "N",
+        },
+        2: {  # accusative
+            "morphgnt2": "A",
+        },
+        3: {  # vocative
+            "morphgnt2": "V",
+        },
+        4: {  # genitive
+            "morphgnt2": "G",
+        },
+        5: {  # dative
+            "morphgnt2": "D",
+        },
+        6: {  # strong ("core")
+            "morphgnt2": "C",
+        },
+        7: {  # weak
+        },
+    },
+    "gender": {
+        1: {  # feminine
+            "morphgnt2": "F",
+        },
+        2: {  # masculine
+            "morphgnt2": "M",
+        },
+        3: {  # neuter
+            "morphgnt2": "N",
+        },
+        4: {  # feminine+masculine+neuter syncretism
+            "morphgnt2": "X",
+        },
+        5: {  # masculine+neuter (non-feminine) syncretism
+            "morphgnt2": "Y",
+        },
+        6: {  # feminine+masculine (animate) syncretism
+            "morphgnt2": "Z",
+        },
+    },
     "number": {
-        1: {
+        1: {  # singular
             "morphgnt2": "S",
         },
-        3: {
+        3: {  # plural
             "morphgnt2": "P",
         },
     },
     "person": {
-        1: {
+        1: {  # first
             "morphgnt2": "1",
         },
-        2: {
+        2: {  # second
             "morphgnt2": "2",
         },
-        3: {
+        3: {  # third
             "morphgnt2": "3",
+        },
+    },
+    "voice": {
+        1: {  # active
+            "morphgnt2": "A",
+        },
+        2: {  # middle ("MP1")
+            "morphgnt2": "M",
+        },
+        3: {  # "passive" ("MP2")
+            "morphgnt2": "P",
+        },
+    },
+    "tense": {
+        1: {  # present
+            "morphgnt2": "P",
+        },
+        2: {  # imperfect
+            "morphgnt2": "I",
+        },
+        3: {  # future
+            "morphgnt2": "F",
+        },
+        4: {  # aorist
+            "morphgnt2": "A",
+        },
+        5: {  # perfect
+            "morphgnt2": "X",
+        },
+        6: {  # pluperfect
+            "morphgnt2": "Y",
+        },
+        7: {  # future perfect
+        },
+    },
+    "mood": {
+        1: {  # indicative
+            "morphgnt2": "I",
+        },
+        2: {  # imperative
+            "morphgnt2": "D",
+        },
+        3: {  # subjunctive
+            "morphgnt2": "S",
+        },
+        4: {  # optative
+            "morphgnt2": "O",
+        },
+        5: {  # infinitive
+            "morphgnt2": "N",
+        },
+        6: {  # participle
+            "morphgnt2": "P",
         },
     },
 }
@@ -47,7 +142,7 @@ def map_features(d, scheme):
     }
 
 
-def parse(scheme, regexes, postag):
+def parse_and_map(scheme, regexes, postag):
     for regex in regexes:
         if m := re.match(regex + "$", postag):
             d = m.groupdict()
@@ -62,7 +157,7 @@ def test_parse(scheme, regexes, test_filename):
     with open(test_filename) as f:
         for line in f:
             postag = line.strip()
-            print(postag, parse(scheme, regexes, postag))
+            print(postag, parse_and_map(scheme, regexes, postag))
 
 
 test_parse("morphgnt2", MORPHGNT2_REGEXES, "examples/morphgnt2.txt")
